b10ab66850e80410926cded85b78c8cc3866ee00
Manuel <manuel.szecsenyi@gmail.com>
2020-06-10 11:11:03 +0200

    added customer seeding data

--- packages/packages_customers.sql
+++ packages/packages_customers.sql
@@ -6,21 +6,6 @@
 /* Description: Responsible for adding a customer and getting the turnover
 /*
 /**********************************************************************/
-
-CREATE TABLE bonus_customers
-(
-    CUSTOMER_ID NUMBER PRIMARY KEY,
-    name VARCHAR2(30),
-    surname VARCHAR2(30),
-    phone_number VARCHAR(20),
-    date_of_birth DATE,
-    date_of_signup DATE,
-    email VARCHAR2(50),
-    card_no Number,
-    customer_no Number
-) ;
-
-
 CREATE OR REPLACE PACKAGE bonus_customers_pkg AS
 
 

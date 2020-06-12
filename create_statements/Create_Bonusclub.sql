fa801e948c9e637b303be9e15cd90becbda43754
Manuel <manuel.szecsenyi@gmail.com>
2020-06-12 20:49:24 +0200

    completed package customers

---
+++ create_statements/Create_Bonusclub.sql
@@ -0,0 +1,233 @@
+/*********************************************************************/
+/**
+/** Table: bonus_costumers
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_customers
+/**
+/*********************************************************************/
+
+  CREATE TABLE bonus_customers
+(
+    CUSTOMER_ID NUMBER,
+    name VARCHAR2(30),
+    surname VARCHAR2(30),
+    phone_number VARCHAR(20),
+    date_of_birth DATE,
+    date_of_signup DATE,
+    email VARCHAR2(50),
+    card_no Number,
+
+    PRIMARY KEY (CUSTOMER_ID)
+);
+   
+/*********************************************************************/
+/**
+/** Table: bonus_tax_rates
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_tax_rates
+/**
+/*********************************************************************/
+
+  CREATE TABLE bonus_tax_rates
+   (	
+    TAX_RATE_ID NUMBER,
+	designation VARCHAR2(30),
+	percentage Number
+   );
+   
+/*********************************************************************/
+/**
+/** Table: bonus_articles
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_articles
+/**
+/*********************************************************************/
+
+
+  CREATE TABLE bonus_articles
+   (	
+     ARTICLE_ID NUMBER,
+	 designation VARCHAR2(30),
+	 unit Number,
+	 net_price Number,
+	 tax_rate_id NUMBER
+   );
+   
+/*********************************************************************/
+/**
+/** Table: bonus_positions
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_positions
+/**
+/*********************************************************************/
+
+  CREATE TABLE bonus_positions
+   (	
+     INVOICE_ID NUMBER,
+	 ARTICLE_ID NUMBER,
+	 unit_price NUMBER,
+	 quantity NUMBER,
+	 position NUMBER
+   );
+
+/*********************************************************************/
+/**
+/** Table: bonus_invoices
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_invoices
+/**
+/*********************************************************************/
+
+  CREATE TABLE bonus_invoices
+   (	
+     INVOICE_ID NUMBER,
+	 costumer_id NUMBER,
+	 subtotal_net NUMBER,
+     timestamp TIMESTAMP,
+	 branch_office_id NUMBER
+   );
+   
+/*********************************************************************/
+/**
+/** Table: bonus_branch_offices
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_branch_offices
+/**
+/*********************************************************************/
+
+  CREATE TABLE bonus_branch_offices
+   (	
+     BRANCH_OFFICE_ID NUMBER,
+	 name Varchar(30),
+	 adress Varchar(50),
+	 postcode NUMBER
+   );
+   
+/*********************************************************************/
+/**
+/** Table: bonus_postcodes
+/** Developer: Kurosh Mehdipour
+/** Description: Create table bonus_postcodes
+/**
+/*********************************************************************/
+
+  CREATE TABLE bonus_postcodes
+   (	
+     PLZ NUMBER,
+	 name Varchar(30)
+   );
+
+
+  --  Constraints for Table bonus_costumer
+--------------------------------------------------------
+
+  ALTER TABLE bonus_customers MODIFY (CUSTOMER_ID NOT NULL ENABLE);
+  ALTER TABLE bonus_customers MODIFY (name NOT NULL ENABLE);
+  ALTER TABLE bonus_customers MODIFY (surname NOT NULL ENABLE);
+  ALTER TABLE bonus_customers MODIFY (date_of_birth NOT NULL ENABLE);
+  ALTER TABLE bonus_customers MODIFY (date_of_signup NOT NULL ENABLE);
+  ALTER TABLE bonus_customers MODIFY (email NOT NULL ENABLE);
+  ALTER TABLE bonus_customers MODIFY (card_no NOT NULL ENABLE);
+  
+  
+  --  Constraints for Table bonus_tax_rates
+--------------------------------------------------------
+
+  ALTER TABLE bonus_tax_rates MODIFY (TAX_RATE_ID NOT NULL ENABLE);
+  ALTER TABLE bonus_tax_rates MODIFY (designation NOT NULL ENABLE);
+  ALTER TABLE bonus_tax_rates MODIFY (percentage NOT NULL ENABLE);
+  ALTER TABLE bonus_tax_rates ADD PRIMARY KEY (TAX_RATE_ID) ENABLE;
+	
+  --  Constraints for Table bonus_postcodes
+--------------------------------------------------------
+
+  ALTER TABLE bonus_postcodes MODIFY (PLZ NOT NULL ENABLE);
+  ALTER TABLE bonus_postcodes MODIFY (name NOT NULL ENABLE);
+  ALTER TABLE bonus_postcodes ADD PRIMARY KEY (PLZ) ENABLE;
+
+  --  Constraints for Table bonus_articles
+--------------------------------------------------------
+
+  ALTER TABLE bonus_articles MODIFY (ARTICLE_ID NOT NULL ENABLE);
+  ALTER TABLE bonus_articles MODIFY (designation NOT NULL ENABLE);
+  ALTER TABLE bonus_articles MODIFY (unit NOT NULL ENABLE);
+  ALTER TABLE bonus_articles MODIFY (net_price NOT NULL ENABLE);
+  ALTER TABLE bonus_articles ADD PRIMARY KEY (ARTICLE_ID) ENABLE;
+	
+  --  Constraints for Table bonus_branch_offices
+--------------------------------------------------------
+
+  ALTER TABLE bonus_branch_offices MODIFY (BRANCH_OFFICE_ID NOT NULL ENABLE);
+  ALTER TABLE bonus_branch_offices MODIFY (name NOT NULL ENABLE);
+  ALTER TABLE bonus_branch_offices MODIFY (adress NOT NULL ENABLE);
+  ALTER TABLE bonus_branch_offices ADD PRIMARY KEY (BRANCH_OFFICE_ID) ENABLE;
+	
+  --  Constraints for Table bonus_invoices
+--------------------------------------------------------
+
+  ALTER TABLE bonus_invoices MODIFY (INVOICE_ID NOT NULL ENABLE);
+  ALTER TABLE bonus_invoices MODIFY (subtotal_net NOT NULL ENABLE);
+  ALTER TABLE bonus_invoices MODIFY (testinger NOT NULL ENABLE);
+  ALTER TABLE bonus_invoices ADD PRIMARY KEY (INVOICE_ID) ENABLE;
+
+  --  Constraints for Table bonus_positions
+--------------------------------------------------------
+
+  ALTER TABLE bonus_positions MODIFY (unit_price NOT NULL ENABLE);
+  ALTER TABLE bonus_positions MODIFY (quantity NOT NULL ENABLE);
+  ALTER TABLE bonus_positions MODIFY (position NOT NULL ENABLE);
+  ALTER TABLE bonus_positions ADD PRIMARY KEY (INVOICE_ID,ARTICLE_ID) ENABLE;
+	
+--  Ref Constraints for Table bonus_articles
+--------------------------------------------------------
+
+  ALTER TABLE bonus_articles ADD FOREIGN KEY (tax_rate_id)
+	  REFERENCES bonus_tax_rates (TAX_RATE_ID)  ENABLE;
+	  
+--  Ref Constraints for Table bonus_branch_offices
+--------------------------------------------------------
+
+  ALTER TABLE bonus_branch_offices ADD FOREIGN KEY (postcode)
+	  REFERENCES bonus_postcodes (PLZ) ENABLE;
+
+--  Ref Constraints for Table bonus_invoices
+--------------------------------------------------------
+
+  ALTER TABLE bonus_invoices ADD FOREIGN KEY (costumer_id)
+	  REFERENCES bonus_customers (CUSTOMER_ID) ENABLE;
+	  
+	 ALTER TABLE bonus_invoices ADD FOREIGN KEY (branch_office_id)
+	  REFERENCES bonus_branch_offices (BRANCH_OFFICE_ID) ENABLE;
+
+--  Ref Constraints for Table bonus_positions
+--------------------------------------------------------
+
+  ALTER TABLE bonus_positions ADD FOREIGN KEY (INVOICE_ID)
+	  REFERENCES bonus_invoices (INVOICE_ID) ENABLE;
+	
+  ALTER TABLE bonus_positions ADD FOREIGN KEY (ARTICLE_ID)
+	  REFERENCES bonus_articles (ARTICLE_ID) ENABLE;
+	
+
+	  
+
+	  
+
+	  
+	  
+
+
+	
+
+
+
+
+
+	
+	
+
+ 
+
+   
+
+   
--- packages/packages_customers.sql
+++ packages/packages_customers.sql
@@ -3,7 +3,7 @@
 /* Filename: packages_customers.pkg
 /* Package: bonus_customers
 /* Developer: Manuel Szecsenyi
-/* Description: Responsible for adding a customer and getting the turnover
+/* Description: Responsible for adding a customer and getting their turnover
 /*
 /**********************************************************************/
 
@@ -16,7 +16,7 @@ CREATE OR REPLACE PACKAGE bonus_customers_pkg AS
                            v_email_in VARCHAR,
                            n_card_no_in Number);
 
-    function get_customer_turnover(n_CUSTOMER_ID_in NUMBER) RETURN NUMBER;
+    function get_customer_turnover(n_CUSTOMER_ID_in IN NUMBER) RETURN NUMBER;
 
 END bonus_customers_pkg;
 /
@@ -42,38 +42,33 @@ CREATE OR REPLACE PACKAGE BODY bonus_customers_pkg AS
          v_surname_in,
          v_phone_number,
          d_date_of_birth_in,
-         CURRENT_DATE,
+         SYSDATE,
          v_email_in,
          n_card_no_in
         );
 
     END;
 
-    function get_customer_turnover (gebdate in date default sysdate) return integer
+    function get_customer_turnover(n_CUSTOMER_ID_in IN NUMBER) RETURN NUMBER
 	as
         n_turnover_out NUMBER;
 		begin
 
-		    -- TODO
-		    n_turnover_out := 8012;
+		    SELECT SUM(SUBTOTAL_NET)
+		    INTO n_turnover_out
+            FROM BONUS_CUSTOMERS
+            JOIN BONUS_INVOICES BI on BONUS_CUSTOMERS.CUSTOMER_ID = BI.COSTUMER_ID
+            WHERE CUSTOMER_ID = n_CUSTOMER_ID_in
+		    GROUP BY CUSTOMER_ID;
 
 		    return n_turnover_out;
+
+		    -- return -1 if no customer is found
+		    EXCEPTION WHEN NO_DATA_FOUND THEN
+		        DBMS_OUTPUT.PUT_LINE('No customer found');
+		        return -1;
 
 		end;
 
 END bonus_customers_pkg;
 /
-
-BEGIN
-    bonus_customers_pkg.add_customer(
-        'Manuel',
-        'Szecsenyi',
-        '+4369911114448',
-        '16.11.1997',
-        'manuel@example.org',
-        1,
-        54948916
-        );
-end;
-
-SELECT * FROM BONUS_CUSTOMERS;

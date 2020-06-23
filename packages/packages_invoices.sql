693c1d6d08dad14c7dc7df3e0e60da6e65155b73
Manuel <manuel.szecsenyi@gmail.com>
2020-06-23 11:26:09 +0200

    added invoice package file

---
+++ packages/packages_invoices.sql
@@ -0,0 +1,8 @@
+/**********************************************************************/
+/*
+/* Filename: packages_invoices.pkg
+/* Package: bonus_invoices
+/* Developer: Manuel Szecsenyi
+/* Description: Responsible for CRU(D) actions on Invoices and their positions
+/*
+/**********************************************************************/
